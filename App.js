e4a6cf4ad7c343a43d8718b135746e971826d0d6
TaliaDiasDuarte <taliadias.adm@gmail.com>
2024-03-23 00:32:03 -0300

    teste

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js talia</Text>
+      <Text>Open up App.js talia teste</Text>
       <StatusBar style="auto" />
     </View>
   );
