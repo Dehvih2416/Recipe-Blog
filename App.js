771b452af055a72fe199c7ce3b065a5b34e74362
TaliaDiasDuarte <taliadias.adm@gmail.com>
2024-04-03 16:24:39 -0300

    alteracao01

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js talia teste</Text>
+      <Text>Open up App.js talia</Text>
       <StatusBar style="auto" />
     </View>
   );
